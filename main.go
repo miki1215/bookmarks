460a8759c0ea4c33cfb972df51fc51a94090978f
miki1215 <66144549+miki1215@users.noreply.github.com>
2023-03-14 12:11:40 +0000

    uu

--- main.go
+++ main.go
@@ -5,10 +5,18 @@ import (
 )
 
 
-type(
+type Bookmark struct{
 	Name string
-)
+	Id int
+	Url string
+}
+
+func (b Bookmark)AddDate() Bookmark{
+return Bookmark{Name: "jj"}
+}
 
 func main() {
-	fmt.Printf("fr")
+	var bbb Bookmark
+	bbb.Id = 7 
+	fmt.Print(bbb.AddDate().Id)
 }
